c2d91b291f3a92e51d160fea1a5fcf906e36382c
seon-mikim <tjsalsun@gmail.com>
2023-05-23 02:57:19 +0900

    feat: Create DarkModeSwitch components

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,6 +3,7 @@ import MenuItem from './MenuItem'
 import {AiFillHome} from 'react-icons/ai'
 import {AiFillInfoCircle} from 'react-icons/ai'
 import Link from 'next/link'
+import DarkModeSwitch from './DarkModeSwitch'
 
 export default function Header() {
   return (
@@ -11,6 +12,7 @@ export default function Header() {
             <MenuItem title='HOME' address='/' Icon={AiFillHome}/>
             <MenuItem title='ABOUT' address='/about' Icon={AiFillInfoCircle}/>
         </div>
+        <DarkModeSwitch/>
         <div className=''>
             <Link href='/'><h2 className='text-2xl'><span className='font-bold bg-cyan-600 py-1 px-2 rounded-[2px] text-white mr-1'>NEXT MOVIE</span><span className='text-sm hidden sm:inline ml-1'>IMDb Clone</span></h2></Link>
         </div>
