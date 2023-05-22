62c629174e0ff87d3c0d74954efb65061cdd7531
seon-mikim <tjsalsun@gmail.com>
2023-05-23 03:07:09 +0900

    design: style css

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -12,8 +12,8 @@ export default function Header() {
             <MenuItem title='HOME' address='/' Icon={AiFillHome}/>
             <MenuItem title='ABOUT' address='/about' Icon={AiFillInfoCircle}/>
         </div>
+        <div className='flex items-center space-x-5'>
         <DarkModeSwitch/>
-        <div className=''>
             <Link href='/'><h2 className='text-2xl'><span className='font-bold bg-cyan-600 py-1 px-2 rounded-[2px] text-white mr-1'>NEXT MOVIE</span><span className='text-sm hidden sm:inline ml-1'>IMDb Clone</span></h2></Link>
         </div>
     </div>
