f0303c425d918c7bc3713f1338ec5406b8d27778
seon-mikim <tjsalsun@gmail.com>
2023-05-28 19:16:50 +0900

    feat: API response 호출 하는 변수 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-import Image from 'next/image'
+const API_KEY = process.env.API_KEY
 
-export default function Home() {
+interface searchParamsProps {
+  searchParams: {
+    genre:string
+  }
+}
 
+
+export default async function Home({searchParams}:searchParamsProps) {
+  const genre = searchParams.genre || "fetchTrending"
+
+  const response = await fetch(`https://api.themoviedb.org/3/
+  ${genre === "fetchTopRated"?"movie/top_rated":"trending/all/week"}
+  ?api_key=${API_KEY}&language=en_US&page=1`)
+  
   return (
     <div className='max-w-6xl mx-auto space-x-4'>
-      <h1 className='text-2xl font-medium text-cyan-600'>HOME</h1>
+      <h1 className='text-2xl font-medium text-cyan-600'>HOME</h1> 
     </div>
   )
 }
