ae086ea0010b1c5b747d8242db37e3b75721cb00
seon-mikim <tjsalsun@gmail.com>
2023-06-20 19:25:25 +0900

    feat: search 기능 구현

--- src/app/search/[searchTerm]/page.tsx
+++ src/app/search/[searchTerm]/page.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import Results from '@/app/components/Results'
 interface SearchParamsProps {
-    searchParams:{
+    params:{
         searchTerm:string
     } 
 }
 
-export default async function SearPage({searchParams}:SearchParamsProps) {
-  const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchParams.searchTerm}&language=en-US&page=1&include_adult=false`)
+export default async function SearchPage({params}:SearchParamsProps) {
+  const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${params.searchTerm}&language=en-US&include_adult=false`)
   if(!response.ok) {
     throw new Error('Error fetching data')
   }
-
+    const data = await response.json()
+    const results = data.results
     return (
-    <div>SearPage</div>
+    <div>
+      {results && results.length ===  0 && (
+      <h1 className='text-center pt-6'>No results 
+      found</h1>
+      )}
+      {results && <Results results={results}/>}
+      </div>
   )
 }
